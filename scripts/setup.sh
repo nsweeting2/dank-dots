b4a6977b7571ede611b1afebd5a443662e5857e2
Noah Sweeting <nsweeting2@gmail.com>
2025-11-12 12:04:43 -0500

    .

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,13 +15,39 @@
 # Trap for unexpected exits
 trap 'trap_message' INT TERM
 
+# Backup default dorfiles then create a symlink for my dotfiles
+# Move file to be a .bak
+mv ~/.bash_profile ~/.bash_profile.bak
+mv ~/.config/DankMaterialShell/settings.json ~/.config/DankMaterialShell/settings.json.bak
+mv ~/.config/ghostty/config ~/.config/ghostty/config.bak
+mv ~/.config/hypr/binds_media.conf ~/.config/hypr/binds_media.conf.bak
+mv ~/.config/hypr/binds.conf ~/.config/hypr/binds.conf.bak
+mv ~/.config/hypr/dwindle.conf ~/.config/hypr/dwindle.conf.bak
+mv ~/.config/hypr/env.conf ~/.config/hypr/env.conf.bak
+mv ~/.config/hypr/monitors.conf ~/.config/hypr/monitors.conf.bak
+mv ~/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf.bak
+# Symlink file from repo
+ln -s ~/.dank-dots/bash_profile ~/.bash_profile
+ln -s ~/.dank-dots/.config/DankMaterialShell/settings.json ~/.config/DankMaterialShell/settings.json
+ln -s ~/.dank-dots/.config/ghostty/config ~/.config/ghostty/config
+ln -s ~/.dank-dots/.config/hypr/binds_media.conf ~/.config/hypr/binds_media.conf
+ln -s ~/.dank-dots/.config/hypr/binds.conf ~/.config/hypr/binds.conf
+ln -s ~/.dank-dots/.config/hypr/dwindle.conf ~/.config/hypr/dwindle.conf
+ln -s ~/.dank-dots/.config/hypr/env.conf ~/.config/hypr/env.conf
+ln -s ~/.dank-dots/.config/hypr/monitors.conf ~/.config/hypr/monitors.conf
+ln -s ~/.dank-dots/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+
+# Copy over the all of the wallpapers
+mkdir ~/Wallpapers
+cp -rp ~/.dank-dots/Wallpapers/* ~/Wallpapers 
+
 # List of AUR packages to install with YAY
 packages=(
     "vivaldi"
-    "package2"
-    "package3"
-    "package4"
-    # Add more packages as needed
+    "chromium"
+    "vscodium"
+    "nano"
+    "btop"
 )
 
 # Install Yay if it is not already installed.
